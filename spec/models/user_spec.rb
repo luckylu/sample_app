95985ec411b09db8bdeccd63d73e0b07080b8535
Michael Lu <646179989@qq.com>
2014-07-03 22:25:50 +0800

    aa

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ describe User do
   		             password_confirmation: "foobar") 
   end
 subject { @user }
+
 it { should respond_to(:name) }
 it { should respond_to(:email) }
 it { should respond_to(:password_digest) }
@@ -15,6 +16,21 @@ it { should respond_to(:password) }
 it { should respond_to(:password_confirmation) }
 it { should respond_to(:authenticate) }
 it { should respond_to(:remember_token) }
+ it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+  	before do
+  		@user.save!
+  		@user.toggle!(:admin)
+  	end
+
+  	it{ should be_admin }
+  end
+
 
 describe "remember_token" do
 	before { @user.save }
